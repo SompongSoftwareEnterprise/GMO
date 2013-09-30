b551dc018f4014704ba6fdd821f3d0bc9a60deca
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-09-30 09:13:08 +0700

    create the users table

---
+++ gmo/app/database/migrations/2013_09_29_145127_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table) {
+			$table->increments('id');
+			$table->string('username')->unique();
+			$table->string('password_hash');
+			$table->string('password_salt');
+			$table->string('name');
+			$table->string('role');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
