2a48e2a063f17b85dea172191f72089f3566f233
armkanin1 <arm.se.th@gmail.com>
2013-10-09 14:37:26 +0700

    update lab controller

---
+++ gmo/app/controllers/LabController.php
@@ -0,0 +1,74 @@
+<?php
+
+class LabController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function show($id) {
+		$labtask = LabTask::where('reference_id' , '=', $id)->first();
+		$labtaskProduct = LabTaskProduct::find($labtask->id);
+		$labtaskAssignment = LabTaskAssignment::find($labtask->id);
+	}
+
+
+	public function index() {
+
+		$labtasks = LabTask::all();
+
+		$labtasks_not_finish = $labtasks->filter(function($task) {
+			if($task->status != 'Waiting For Approval') {
+				return $task;
+			}
+		});
+
+		$labtasks_waiting_for_approval = $labtasks->filter(function($task) {
+			if($task->status == 'Waiting For Approval') {
+				return $task;
+			}
+		});
+
+		$labtasks_not_finish = $this->sortByDueDate($labtasks_not_finish);
+
+		$labtasks_waiting_for_approval = $this->sortByDueDate($labtasks_waiting_for_approval);
+
+		$items_not_finish = $this->getTableItem($labtasks_not_finish);
+		$items_waiting_for_approval = $this->getTableItem($labtasks_waiting_for_approval);
+
+
+		return View::make('labtask/index')->with('items',array($items_not_finish,$items_waiting_for_approval));
+	}
+
+	private function sortByDueDate($labtasks) {
+		$labtasks = $labtasks->sortBy(function($labtask)
+		{
+			$product = LabTaskProduct::find($labtask->id);
+		    return $product->finish;
+		});
+
+		return $labtasks;
+	}
+
+	private function getTableItem($labtasks) {
+		$items = array();
+		foreach ($labtasks as $labtask) {
+
+			$labproduct = LabTaskProduct::find($labtask->id);
+			$item = array('taskid' => $labtask->reference_id,'taskname' => $labproduct->product_name,'duedate' => $labproduct->finish,'status' => $labtask->status);
+			array_push($items, $item);
+		}
+
+		return $items;
+	}
+	
+}
