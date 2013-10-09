167cb7ccaac0c742fe4d6de1b481cd4fb4d47ca2
armkanin1 <arm.se.th@gmail.com>
2013-10-09 14:33:21 +0700

    Add php code to fill table

--- gmo/app/views/labtask/index.blade.php
+++ gmo/app/views/labtask/index.blade.php
@@ -6,8 +6,6 @@ Registration
 
 @section('content')
 
-{{dd($items)}}
-
 <div class="row">
 	<div class="col-sm-12">
 		<ul id="labTaskTab" class="nav nav-tabs">
@@ -29,45 +27,18 @@ Registration
 						</tr>
 					</thead>
 					<tbody>
-						foreach ($labtasks as $labtask) {
-
-						}
-						<tr>
-							<td><a href="#">0001</a></td>
-							<td>Task name 0001</td>
-							<td>dd/mm/yyyy</td>
-							<td class="text-warning">Pending</td>
-						</tr>
-						<tr>
-							<td><a href="#">0002</a></td>
-							<td>Task name 0002</td>
-							<td>dd/mm/yyyy</td>
-							<td class="text-warning">Pending</td>
-						</tr>
-						<tr>
-							<td><a href="#">0003</a></td>
-							<td>Task name 0003</td>
-							<td>dd/mm/yyyy</td>
-							<td class="text-warning">Pending</td>
-						</tr>
-						<tr>
-							<td><a href="#">0004</a></td>
-							<td>Task name 0004</td>
-							<td>dd/mm/yyyy</td>
-							<td>Analyzing</td>
-						</tr>
-						<tr>
-							<td><a href="#">0005</a></td>
-							<td>Task name 0005</td>
-							<td>dd/mm/yyyy</td>
-							<td>Analyzing</td>
-						</tr>
-						<tr>
-							<td><a href="#">0006</a></td>
-							<td>Task name 0006</td>
-							<td>dd/mm/yyyy</td>
-							<td>Analyzing</td>
-						</tr>
+						@foreach ($items[0] as $labtask) 
+							<tr>
+							<td><a href="#">{{$labtask['taskid']}}</a></td>
+							<td>{{$labtask['taskname']}}</td>
+							<td>{{$labtask['duedate']}}</td>
+							@if ($labtask['status'] == 'Pending')
+								<td class="text-warning">{{$labtask['status']}}</td>
+							@else
+								<td>{{$labtask['status']}}</td>
+							@endif
+							</tr>
+						@endforeach
 					</tbody>
 				</table>
 
@@ -86,24 +57,14 @@ Registration
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td><a href="#">0011</a></td>
-							<td>Task name 0001</td>
-							<td>dd/mm/yyyy</td>
-							<td class="text-warning">Waiting For Approval</td>
-						</tr>
-						<tr>
-							<td><a href="#">0012</a></td>
-							<td>Task name 0002</td>
-							<td>dd/mm/yyyy</td>
-							<td class="text-warning">Waiting For Approval</td>
-						</tr>
-						<tr>
-							<td><a href="#">0013</a></td>
-							<td>Task name 0003</td>
-							<td>dd/mm/yyyy</td>
-							<td class="text-warning">Waiting For Approval</td>
-						</tr>
+						@foreach ($items[1] as $labtask) 
+							<tr>
+							<td><a href="#">{{$labtask['taskid']}}</a></td>
+							<td>{{$labtask['taskname']}}</td>
+							<td>{{$labtask['duedate']}}</td>
+							<td class="text-warning">{{$labtask['status']}}</td>
+							</tr>
+						@endforeach
 					</tbody>
 				</table>
 
