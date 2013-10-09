8d06f4816a9e333695d76129b73010302396d10c
Gabfest <Gabfest@MacBook-Pro-khxng-Kittinan.local>
2013-10-09 22:42:07 +0700

    update layout putting header and menu bar update css

--- gmo/app/views/layout.blade.php
+++ gmo/app/views/layout.blade.php
@@ -11,6 +11,42 @@
 </head>
 
 <body>
+<img class="logo" src = "/assets/img/logo.png">
+
+<div class="text">SFKLDF</div>
+<div class="container">
+<nav class="navbar navbar-default " role="navigation">
+  <div class="navbar-header">
+    
+    <!-- <a class="navbar-brand logo-small" href="#"><img  src = "/assets/img/logo_small.png"></a> -->
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">HOME</a></li>
+      <li><a href="#">REQUEST</a></li>
+      <li><a href="#">AGENCY</a></li>
+      
+    </ul>
+
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user">  Entreprenuer</span></a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        	<span class="glyphicon glyphicon-cog"> Setting <b class="caret"></b></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li><a href="#">Log out</a></li>
+
+        </ul>
+      </li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+</div>
 
 <div class="container">
 	
