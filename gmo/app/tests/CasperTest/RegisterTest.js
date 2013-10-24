60abe9fe21029a6f3c38e0b87a9ce1e3a05e8faa
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-10-25 01:47:08 +0700

    Implement URL generation function and update example to use it.

--- gmo/app/tests/CasperTest/RegisterTest.js
+++ gmo/app/tests/CasperTest/RegisterTest.js
@@ -1,8 +1,9 @@
-var url = "http://gmo.tsp.dt.in.th/"
+
+var url = require('./url')
 
 casper.test.begin('Register test as a customer', function suite(test) {
-    casper.start(url, function() {
 
+    casper.start(url('/'), function() {
 
         //check that register button exists
 
@@ -14,29 +15,20 @@ casper.test.begin('Register test as a customer', function suite(test) {
 
     casper.then(function() {
 
-        //TODO now register as customer and agency element have the same information
+        // TODO now register as customer and agency element have the same information
         // Should have ID ???????
 
         test.assertExists('#register-customer','Check button for register as Customer')
+
     });
 
     // HARD CODE !!!!!  ->  url register as customer
-    casper.thenOpen('http://gmo.tsp.dt.in.th/staff/register/customer', function() {
+    casper.thenOpen(url('/staff/register/customer'), function() {
+
         // Check all element in from should exists
-        test.assertExists({
-            type: 'xpath', 
-            path: '//input[@id="is_company_checkbox" and @type="checkbox"]' 
-        },'company checkbox element exists');
-
-        test.assertExists({
-            type: 'xpath', 
-            path: '//input[@id="first_name" and @type="text"]' 
-        },'first name textbox element exists');
-
-        test.assertExists({
-            type: 'xpath', 
-            path: '//input[@id="last_name" and @type="text"]' 
-        },'last name textbox element exists');
+        test.assertExists('input#is_company_checkbox[type=checkbox]', 'company checkbox element exists');
+        test.assertExists('input#first_name[type=text]', 'first name textbox element exists');
+        test.assertExists('input#last_name[type=text]', 'last name textbox element exists');
 
         // This 2 element not have id !!
         // Should we have id or select with name instead
@@ -51,29 +43,14 @@ casper.test.begin('Register test as a customer', function suite(test) {
         //     path: '//input[@id="is_female_radio" and @type="radio"]' 
         // },'Sex-female radio element exists');
 
-        test.assertExists({
-            type: 'xpath', 
-            path: '//input[@name="date_of_birth__date" and @type="number"]' 
-        },'date of birth - date number element exists');
-
-        test.assertExists({
-            type: 'xpath', 
-            path: '//select[@name="date_of_birth__month"]' 
-        },'date of birth - month number element exists');
-
-        test.assertExists({
-            type: 'xpath', 
-            path: '//input[@name="date_of_birth__year" and @type="number"]' 
-        },'date of birth - year number element exists');
-
-        test.assertExists({
-            type: 'xpath', 
-            path: '//input[@id="nationality" and @type="text"]' 
-        },'nationality textbox element exists');
+        test.assertExists('input[name=date_of_birth__date][type=number]', 'date of birth - date number element exists');
+        test.assertExists('select[name=date_of_birth__month]',            'date of birth - month number element exists');
+        test.assertExists('input[name=date_of_birth__year][type=number]', 'date of birth - date number element exists');
 
     });
 
     casper.run(function() {
         test.done();
     });
+
 });
---
+++ gmo/app/tests/CasperTest/url.js
@@ -0,0 +1,6 @@
+
+function url(path) {
+	return 'http://gmo.tsp.dt.in.th' + path
+}
+
+module.exports = url
