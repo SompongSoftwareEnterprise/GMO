a3890614693804f5e2df70110c7a8a410f15dbce
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-11-04 10:12:56 +0700

    More refactoring and human readable comments

--- casper/RegisterTest.js
+++ casper/RegisterTest.js
@@ -2,19 +2,21 @@
 var url = require('./helpers/url')
 var yaml = require('./helpers/yaml')
 
-var testdata = yaml('testdata/register.yml')
-
 /**
- * Register test - Tests that the registration process is working and
- * an entrepreneur is registered successfully and can login.
+ * To make sure that the registration process is working correctly.
  *
- * @fixture fixtures/clear-users.yml
+ * @xref     uc101, ds101, ds102
+ * @actors   GMO Staff
+ * @pre      GMO staff is logged in.
+ * @fixture  fixtures/clear-users.yml
  */
 casper.test.begin('Register test as a customer', function suite(test) {
 
 	casper.start(url('/staff/register'))
 	
 	casper.then(function() {
+		test.comment('Enter the registration page.')
+		test.comment('Click the register customer button.')
 		casper.click('#register-customer')
 	})
 
@@ -22,23 +24,23 @@ casper.test.begin('Register test as a customer', function suite(test) {
 	// must be disabled.
 	casper.then(function() {
 
-		test.comment('When checking "is company", some fields must be disabled')
-		casper.click('#is_company_checkbox')
-		test.assertExists('#last_name:disabled')
-		test.assertExists('input[name=sex]:disabled')
-		test.assertExists('#date_of_birth:disabled')
+		test.assertExists('form#register-form',             'A form must be shown.')
 
-		test.comment('When unchecking "is company", these fields must be re-enabled')
+		test.comment('When checking the "is company" checkbox:')
 		casper.click('#is_company_checkbox')
-		test.assertDoesntExist('#last_name:disabled')
-		test.assertDoesntExist('input[name=sex]:disabled')
-		test.assertDoesntExist('#date_of_birth:disabled')
+		test.assertExists('#last_name:disabled',            'Last name field must be disabled.')
+		test.assertExists('input[name=sex]:disabled',       'Sex radio boxes must be disabled.')
+		test.assertExists('#date_of_birth:disabled',        'Date of birth field must be disabled.')
 
-		test.comment('Now filling forms with few invalid data.')
+		test.comment('When unchecking "is company" checkbox:')
 		casper.click('#is_company_checkbox')
-		casper.fill('form#register-form', testdata.customer_try1, true)
+		test.assertDoesntExist('#last_name:disabled',       'Last name field must be re-enabled.')
+		test.assertDoesntExist('input[name=sex]:disabled',  'Sex radio boxes must be re-enabled.')
+		test.assertDoesntExist('#date_of_birth:disabled',   'Date of birth field must be re-enabled.')
 
-		test.comment('Waiting for error box')
+		test.comment('Fill and submit the form with almost complete data from [[register_customer_attempt1]].')
+		casper.click('#is_company_checkbox')
+		casper.fill('form#register-form', yaml.testdata('register_customer_attempt1'), true)
 
 	})
 
@@ -46,23 +48,24 @@ casper.test.begin('Register test as a customer', function suite(test) {
 
 	casper.then(function() {
 
-		// casper.echo(casper.fetchText('.error-box li'))
-		test.assertExists('.error-box', 'Error box is shown')
-		test.comment('Now completing the form with valid data.')
-		casper.fill('form#register-form', testdata.customer_try2, true)
+		test.assertExists('.error-box', 'An error box must be shown.')
 
-		test.comment('Waiting for success message box')
+		test.comment('Complete the remaining/incorrect fields with the data from [[register_customer_attempt2]].')
+		casper.fill('form#register-form', yaml.testdata('register_customer_attempt2'), true)
 
 	})
 
 	casper.waitForSelector('.message-box')
 
 	casper.then(function() {
-		test.assertExists('.message-box', 'Message box is shown')
-		test.comment('Register finished.')
+		test.assertExists('.message-box',
+			'A message box that the registration is complete must be shown.')
+		test.comment('Click the Finish button.')
+		casper.click('.message-primary-action')
 	})
 
 	casper.run(function() {
+		test.assertExists('#register-customer', 'The registration home page must be shown.')
 		test.done()
 	})
 
