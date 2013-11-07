62842711a80c04e95ceab55500b61a91a892abfa
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2013-11-07 10:58:51 +0700

    Generate reference ID, and log outputs to XML file.

--- casper/helpers/suite.js
+++ casper/helpers/suite.js
@@ -2,19 +2,50 @@
 
 var url = require('./url')
 var yaml = require('./yaml')
+var screenshot = require('./screenshot')
+
+casper.setFilter('echo.message', function alterEchoMessage(message) {
+	return message.replace(/<ref id=\d+>/g, '')
+})
 
 function CasperAPI(casper) {
 	
 	var test = null
 	var api = { }
 
+	var nextRefId = 1
+	function refId() {
+		return '<ref id=' + (nextRefId++) + '>'
+	}
+
+
+	/**
+	 * [Util] Define a step.
+	 */
+	var stepNumber = 0
+	api.step = function(message) {
+		message += refId()
+		casper.then(function() {
+			test.comment(++stepNumber + '. ' + message)
+		})
+	}
+
+	/**
+	 * [Util] Define a checking criteria.
+	 */
+	api.check = function(message, callback) {
+		message += refId()
+		return function() {
+			callback(message)
+		}
+	}
+
+
 	/**
 	 * [Step] Navigate to the `path`.
 	 */
 	api.go = function(path, message) {
-		casper.then(function() {
-			api.step(message)
-		})
+		api.step(message)
 		casper.thenOpen(url(path))
 	}
 
@@ -22,10 +53,8 @@ function CasperAPI(casper) {
 	 * [Step] Login with specified `username` and `password`.
 	 */
 	api.login = function(username, password) {
+		api.step('Login with username="' + username + '" and password="' + password + '"')
 		casper.thenOpen(url('/'))
-		casper.then(function() {
-			api.step('Login with username="' + username + '" and password="' + password + '"')
-		})
 		casper.then(function() {
 			casper.fill('#login-form', {
 				username: username,
@@ -33,43 +62,36 @@ function CasperAPI(casper) {
 			})
 			casper.click('#login-form .btn-primary')
 		})
-		casper.then(function() {
-			test.assertDoesntExist('#login-errors', 'Must login successfully')
-		})
+		casper.then(api.check('Must login successfully', function(message) {
+			test.assertDoesntExist('#login-errors', message)
+		}))
 	}
 
-	/**
-	 * [Util] Log a certain step.
-	 */
-	var stepNumber = 0
-	api.step = function(message) {
-		test.comment(++stepNumber + '. ' + message)
-	}
 
 	/**
 	 * [Check] Check that an element exists.
 	 */
 	api.assertExists = function(selector, message) {
-		casper.then(function() {
-			test.assertExists(selector, message)
-		})
+		casper.then(api.check(message, function(_message) {
+			test.assertExists(selector, _message)
+		}))
 	}
 
 	/**
 	 * [Check] Check that an element does not exist.
 	 */
 	api.assertDoesntExist = function(selector, message) {
-		casper.then(function() {
-			test.assertDoesntExist(selector, message)
-		})
+		casper.then(api.check(message, function(_message) {
+			test.assertDoesntExist(selector, _message)
+		}))
 	}
 
 	/**
 	 * [Step] Click an element and display a message.
 	 */
 	api.click = function(selector, message) {
+		api.step(message)
 		casper.then(function() {
-			api.step(message)
 			this.click(selector)
 		})
 	}
@@ -78,8 +100,8 @@ function CasperAPI(casper) {
 	 * [Step] Fill the form and click submit.
 	 */
 	api.fillAndSubmit = function(form, data, message) {
+		api.step(message.replace(':name', data))
 		casper.then(function() {
-			api.step(message.replace(':name', data))
 			this.fill(form, yaml.testdata(data), true)
 		})
 	}
@@ -88,13 +110,10 @@ function CasperAPI(casper) {
 	 * [Check] Check that the form has the correct value
 	 */
 	api.assertFormValue = function(form, name, value) {
-		casper.then(function() {
-			test.assertEquals(
-				casper.getFormValues(form)[name],
-				value,
-				'The field ' + name + ' in ' + form + ' must have value ' + value
-			)
-		})
+		var message = 'The field ' + name + ' in ' + form + ' must have value ' + value
+		casper.then(api.check(message, function(_message) {
+			test.assertEquals(casper.getFormValues(form)[name], value, _message)
+		}))
 	}
 
 	/**
@@ -110,9 +129,11 @@ function CasperAPI(casper) {
 	 */
 	api.run = function(fn, name) {
 
-		casper.test.begin('== ' + name + ' ==', function(tester) {
+		casper.test.begin(name, function(tester) {
 
 			test = tester
+			setupTest()
+
 			casper.start()
 			casper.options.pageSettings.loadImages = false
 			fn(api)
@@ -124,19 +145,37 @@ function CasperAPI(casper) {
 
 	}
 
+	/**
+	 * Monkey-patches CasperJS to save screenshot on each failure
+	 */
+	function setupTest() {
+
+		override(test.currentSuite, 'addFailure', after(function(failure, time) {
+
+			console.log('[Capturing screenshot...]')
+			screenshot.capture(casper)
+			console.log('Screenshot saved at ' + screenshot.path)
+
+			var error = getLaravelError()
+
+			if (error) {
+				console.log('Laravel has something to say:')
+				console.log(error)
+			}
+
+		}))
+
+	}
+
 	return api
 
 }
 
-var screenshot = require('./screenshot')
-
-casper.test.on('fail', function(failure) {
-
-	console.log('[Capturing screenshot...]')
-	screenshot.capture(casper)
-	console.log('Screenshot saved at ' + screenshot.path)
-
-	var error = casper.evaluate(function getErrorMessage() {
+/**
+ * Scrapes the Laravel error message from the web page.
+ */
+function getLaravelError() {
+	return casper.evaluate(function getErrorMessage() {
 		function text(el) {
 			if (!el) return ''
 			return el.innerText.trim()
@@ -147,66 +186,46 @@ casper.test.on('fail', function(failure) {
 		if (!excTitle) return null
 		return text(excTitle) + ': ' + text(excMessage) + ' at ' +
 			text(file)
-			
 	})
-
-	if (error) {
-		console.log('Laravel has something to say:')
-		console.log(error)
-	}
-
-})
-
-function ExportAPI() {
-
-	var api = { }
-	var stepNumber = 0
-
-	function step(text) {
-		casper.echo(++stepNumber + '. ' + text)
-	}
-	function check(message) {
-		casper.echo('   * ' + message)
-	}
-
-	api.go = function(path) {
-		step('Go to ' + path)
-	}
-	api.assertExists = function(selector, message) {
-		check(message)
-	}
-	api.assertDoesntExist = function(selector, message) {
-		check(message)
-	}
-	api.click = function(selector, message) {
-		step(message)
-	}
-	api.fillAndSubmit = function(form, data, message) {
-		step(message.replace(':name', '`' + data + '`'))
-		casper.echo('')
-		casper.echo('   | Field Name | Value |')
-		casper.echo('   | ---------- | ----- |')
-		var d = yaml.testdata(data)
-		for (var i in d) {
-			casper.echo('   | ' + i + ' | ' + d[i] + ' |')
-		}
-		casper.echo('')
-	}
-	api.wait = function(selector, message) {
-		api.assertExists(selector, message)
-	}
-	api.run = function(fn, name) {
-		casper.echo('# ' + name)
-		fn(api)
-	}
-
-	return api
-
 }
 
+/**
+ * Overrides a method in an object.  Usage:
+ *
+ *		override(someObject, 'method', function(_super) {
+ *			// _super is a reference to the original function
+ *			return function myFunction() {
+ *				// when someObject.method is invoked, it will call myFunction instead.
+ *				// you can call super like this: _super.apply(this, arguments)
+ *			}
+ *		})
+ */
+function override(object, method, fn) {
+	object[method] = fn(object[method])
+}
+
+/**
+ * This function runs the specified function after the previous function
+ * is run.  Usage:
+ *
+ *		override(someObject, 'method', after(function myFunction() {
+ *			// when someObject.method is invoked, it will call the original
+ *			// function, and then myFunction is called afterwards.
+ *		})
+ */
+function after(fn) {
+	return function(_super) {
+		return function() {
+			var result = _super.apply(this, arguments)
+			fn.apply(this, arguments)
+			return result
+		}
+	}
+}
 
 /*global casper*/
 module.exports = function suite(name, fn) {
 	var api = CasperAPI(casper)
 	api.run(fn, name)
 }
+
