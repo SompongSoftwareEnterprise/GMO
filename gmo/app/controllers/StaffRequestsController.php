799fd2e3d92cc282fcc2839bca2bf41d64e208cc
armkanin1 <arm.se.th@gmail.com>
2013-12-04 17:50:20 +0700

    view infomation bug fix

--- gmo/app/controllers/StaffRequestsController.php
+++ gmo/app/controllers/StaffRequestsController.php
@@ -23,8 +23,10 @@ class StaffRequestsController extends BaseController {
 		if($form == '11') {
 			$request = CertificateRequest::where('reference_id', '=', $id)->first();
 			$user = User::find($request->signer_id);
+			$form = CertificateRequestForm::where('reference_id', '=', $id)->first();
+			$example = CertificateRequestExample::where('reference_id', '=', $id)->get();
 			$entrepreneur = Entrepreneur::where('user_id', '=', $user['id'])->first();
-			return View::make('view-form-1-1-1')->with('ex_cert',$request)->with('entrepreneur',$entrepreneur);
+			return View::make('view-form-1-1-1')->with('entrepreneur',$entrepreneur)->with('ex_cert',$form)->with('example',$example);
 		}
 		else if($form == '12') {
 
