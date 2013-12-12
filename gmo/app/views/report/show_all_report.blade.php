a78f38905d7c939d52b6b86f7265e2a3eff0da4b
Gabfest <Gabfest@MacBook-Pro-khxng-Kittinan.local>
2013-12-12 14:49:06 +0700

    EDIT report page

--- gmo/app/views/report/show_all_report.blade.php
+++ gmo/app/views/report/show_all_report.blade.php
@@ -10,12 +10,22 @@ View Report
             <tr class="header" >
                 <th>#</th>
                 <th>Report</th>
+                <th>Start Period</th>
+                <th>End Period</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td>1</td>
-                <td><a href="report/1">Domestic Plant (1/1/2013 - 31/12/2013) </a></td>
+                <td><a href="report/1">Domestic Plant</a></td>
+                <td>01/01/2013</td>
+                <td>31/12/2013</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td><a href="report/1">Lab Result</a></td>
+                <td>01/01/2013</td>
+                <td>31/12/2013</td>
             </tr>
         </tbody>
         </table>
